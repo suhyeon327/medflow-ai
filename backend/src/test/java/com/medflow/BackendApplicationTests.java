740d30add3031d95ca09191d49456557ba6c202d
Jang SuHyeon <jshh020327@gmail.com>
2026-07-21 01:43:01 +0900

    fix: gradle clean build 시 테스트 실패 문제 해결

--- backend/src/test/java/com/medflow/BackendApplicationTests.java
+++ backend/src/test/java/com/medflow/BackendApplicationTests.java
@@ -2,8 +2,10 @@ package com.medflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BackendApplicationTests {
 
 	@Test
